83dd7915c4133459c46e59320aa2e41ac4969fd7
lottmarcos <lott.marcos@gmail.com>
2022-06-27 20:10:19 -0300

    save on 06/27/2022 20:10

--- src/main.cpp
+++ src/main.cpp
@@ -176,6 +176,7 @@ int main(int argc, char **argv) {
    defineFaseMemLog(2); // MEMLOG PARA ORDENAÇÃO DAS PALAVRAS
    //ordena o array de words
    //quicksort(palavras, array_counter, M, S);
+   //insertsort(palavras, array_counter);
    selectionsort(palavras, array_counter);
 
    //imprime o resultado
